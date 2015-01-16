a5a000059d1ddd3c640669ce687c0cec66c10942
juhahero <juhahero@gmail.com>
2015-01-17 01:54:04 +0900

    [iProject][race-condition][iWorkQ] applying race-condition
    [Desc] applying race-conditon

--- iNotifier/iWorkQ.py
+++ iNotifier/iWorkQ.py
@@ -1,13 +1,16 @@
-# This is workqueue
+# This is workqueue(FIFO)
 # using Producer/Consumer pattern
+import threading
 
-class WorkQueue :
+class iWorkQ :
     
     pool = ''
     POOL_MAX = 10
     
-    def __WorkQ__(self, LinkedList) :
-        pool = LinkedList 
+    def __init__(self, LinkedList) :
+        pool = LinkedList
+        self.mutex = thread.Lock()
+        self.condition = threading.Condition(self.mutex)
     
     def enqueue(self, obj) :
         global POOL_MAX
@@ -16,18 +19,18 @@ class WorkQueue :
         while (self.size() >= POOL_MAX) :
             try :
                 print "wait enqueue...."
-                wait()
+                self.condition.wait()
             except InterruptedException, msg:
                 print 'InterruptedException : ', msg
             else :
                 pass
         
         
-        pool.add(obj)
+        pool.append(obj)
         
-        if (size() > 0) :
+        if (self.size() > 0) :
             print "notify enqueue...."
-            notifyAll()
+            self.condition.notifyAll()
     
     def dequeue(self) :
         global POOL_MAX
@@ -36,22 +39,22 @@ class WorkQueue :
         while (self.size() == 0) :
             try :
                 print "wait dequeue...."
-                wait()
+                self.condition.wait()
             except InterruptedException, msg :
                 
                 print 'InterruptedException : ', msg
             else :
                 pass
         
-        obj = pool.removeFirst()
+        obj = pool.pop(0)
         if (self.size() < POOL_MAX) :
-            System.out.println("notify dequeue....")
-            notifyAll()
+            print "notify dequeue...."
+            self.condition.notifyAll()
         
         return obj
     
     def size(self) :
-        return pool.size()
+        return pool.count()
     
     def isEmpty(self) :
         if (self.size() == 0) :
