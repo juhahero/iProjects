3097a469865fe457c11b19a721a4376d858b8139
juhahero <juhahero@gmail.com>
2015-01-20 06:57:39 +0900

    [iProject][twitter][iNotifier] implemented to notify msg
    [Desc] It's implemented by using twitter API for notifying threshold

--- iNotifier/iMonitor.py
+++ iNotifier/iMonitor.py
@@ -1,2 +1,19 @@
 # This is for checking threshold
+# using Observer pattern.# This is for checking threshold
 # using Observer pattern.
+
+class iMonitor :
+    def __init__(self, threshold) :
+        self.MAX_THRESHOLD = threshold
+
+    def checkThreshold(self, items, thold) :
+        bRes = False
+        for key, value in items :
+            if(value >= thold) :
+                print 'overheat'
+                return True
+            else :
+                print 'Not over'
+        return bRes
+
+    
--- iNotifier/iTwit.py
+++ iNotifier/iTwit.py
@@ -1,1 +1,31 @@
 # This is notifier
+# This is notifier by using twitter
+
+from tweepy import *
+
+class iTwit :
+    def __init__(self, user) :
+        # consumer login
+        self.consumer_key = "iJtae8bcIzlbaapPp0bob0Kzy"
+        self.consumer_secret = "gUdJiyNSSKGpYuWNv1oT9Jqe9b9f99ZwCXE5qPm2cuqs99BlvN"
+
+        # consumer login
+        self.consumer_key = "iJtae8bcIzlbaapPp0bob0Kzy"
+        self.consumer_secret = "gUdJiyNSSKGpYuWNv1oT9Jqe9b9f99ZwCXE5qPm2cuqs99BlvN"
+
+        # access_token login
+        self.ACCESS_KEY = "177153021-FyGAaxONJtVfzVDxJBMOu6cEM0NyovcmlMFhGXGK"
+        self.ACCESS_SECRET = "IaMxGIikwfP43NJvk1TXOYCckOqCkEZ0Qb0egJW3efIvl"
+ 
+    # send test-DM
+    def sendDM(self, msg) :
+        auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.ACCESS_KEY, self.ACCESS_SECRET)
+        api = API(auth)
+        api.send_direct_message(user='doradoli', text=msg)
+
+'''
+def test() :
+    itwit = iTwit('doradoli')
+test()    
+'''
